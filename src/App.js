c7ada22bad13b7578f7daced69285e6aa99e089a
JavierGold <567498jav@gmail.com>
2022-01-30 16:55:37 -0600

    fixed travis file 3

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,9 +7,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hola soy javi chavos como estan eeee chavos jeje eeee ssee holii
-        </p>
+        <p>Bye there!</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
